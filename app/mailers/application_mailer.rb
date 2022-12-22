a67e57aa86e3778b13580f8b1f6d9b93589acb1e
Kai <hulloitskai@gmail.com>
2022-12-22 10:18:19 -0500

    Fix mailer reply-to address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "Popshop <popshop@itskai.me>",
-          reply_to: "Popshop Support <hello@itskai.me>"
+  default from: "Popshop <popshop@itskai.me>"
   layout "mailer"
 end
